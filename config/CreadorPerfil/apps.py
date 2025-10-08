4e372f03b0f911808a2a91057c84c7bc4e2957f1
BenjaminSepul <benjamin.sepulveda.lobos@gmail.com>
2025-10-08 18:02:40 -0300

    Creación de marco de las apps

---
+++ config/CreadorPerfil/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CreadorperfilConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CreadorPerfil'
--- config/config/settings.py
+++ config/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'miapp',
+    'Busqueda',
+    'CreadorPerfil',
+    'GestionPerfil',
+    'GestionPosts',
 ]
 
 MIDDLEWARE = [
