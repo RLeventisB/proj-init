651fde8103726737a019d000fd9a08a810930aa8
BenjaminSepul <benjamin.sepulveda.lobos@gmail.com>
2025-10-10 21:06:28 -0300

    Base de datos creada y funcionando

--- config/GestionPerfil/models.py
+++ config/GestionPerfil/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuarios(models.Model):
+    correo = models.EmailField(primary_key=True)
+    nombre = models.CharField(max_length=15)
+    contraseña = models.CharField(max_length=10)
+    rango = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
--- config/GestionPosts/models.py
+++ config/GestionPosts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from GestionPerfil.models import Usuarios
 
 # Create your models here.
+
+class Tags(models.Model):
+    tag = models.CharField(max_length=15, primary_key=True)
+
+    def __str__(self):
+        return self.tag
+    
+class Publicaciones(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=40)
+    nombre = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    resumen = models.CharField(max_length=500)
+    parrafo = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='imagenes/', null=True, blank=True)
+    tags = models.ManyToManyField(Tags, blank=True)
+
+    def __str__(self):
+        return self.titulo
+    
+class Comentarios(models.Model):
+    publicacion = models.ForeignKey(Publicaciones, on_delete=models.CASCADE)
+    correo = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_comentario = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comentario by {self.correo} on {self.publicacion}'
+
